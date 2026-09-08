1b5a61bc7e54b549b5697ece280002a8a3936913
Aquaxyy <3596767508@qq.com>
2026-09-08 09:35:33 +0800

    完成首页基础布局

--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/components/Home/HmCategory.dart';
+import 'package:hm_shop/components/Home/HmMoreList.dart';
+import 'package:hm_shop/components/Home/HmSlider.dart';
+import 'package:hm_shop/components/Home/HmSuggestion.dart';
+import 'package:hm_shop/components/Home/HmHot.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -8,8 +13,35 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  //获取滚动容器的内容
+  List<Widget> _getScrollContent() {
+    return [
+      SliverToBoxAdapter(child: HmSlider()), //轮播图组件
+      SliverToBoxAdapter(child: SizedBox(height: 10)), //放置间距组件
+      SliverToBoxAdapter(child: HmCategory()), //分类组件
+      SliverToBoxAdapter(child: SizedBox(height: 10)), //放置间距组件
+      SliverToBoxAdapter(child: HmSuggestion()), //推荐组件
+      SliverToBoxAdapter(child: SizedBox(height: 10)), //放置间距组件
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Flex(
+            direction: Axis.horizontal,
+            children: [
+              Expanded(child: HmHot()),
+              SizedBox(width: 10),
+              Expanded(child: HmHot()),
+            ],
+          ),
+        ),
+      ),
+      SliverToBoxAdapter(child: SizedBox(height: 10)), //放置间距组件
+      HmMoreList(),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('首页'));
+    return CustomScrollView(slivers: _getScrollContent());
   }
 }

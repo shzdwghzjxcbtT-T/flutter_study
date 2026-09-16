69618fa0beba7ff49eef2e6d886973f25b455185
Aquaxyy <3596767508@qq.com>
2026-09-16 22:59:41 +0800

    登录页面搭建和表单校验

--- lib/utils/Toastutils.dart
+++ lib/utils/Toastutils.dart
@@ -2,10 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Toastutils {
+  static bool showLoading = false;
   static void showToast(BuildContext context, String? msg) {
+    if (Toastutils.showLoading) {
+      return;
+    }
+    Toastutils.showLoading = true;
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 120,
+        width: 180,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(40),
         ),
